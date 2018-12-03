52e45590ec50946c88093d26531d2cdaeb930404
HongjianZhu <zhu.life@gmail.com>
2018-12-03 17:06:29 +0800

    fix syntax

--- init.el
+++ init.el
@@ -77,7 +77,7 @@
 (add-hook 'go-mode-hook (lambda ()
 						  (company-mode)
 						  (go-eldoc-setup)
-						  (local-set-key (kbd "M-." 'godef-jump)
+						  (local-set-key (kbd "M-.") 'godef-jump)
 						  (local-set-key (kbd "C-c d") 'godoc)
 						  (local-set-key (kbd "C-c i") 'go-goto-imports)
 						  (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)
